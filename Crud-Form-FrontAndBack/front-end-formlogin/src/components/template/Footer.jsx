c204b8d159927a898cffb1ec7af40efbc7b00510
Rafael Teixeira <rafael.amtx@gmail.com>
2019-09-12 05:14:28 -0300

    front-end and back-end

---
+++ Crud-Form-FrontAndBack/front-end-formlogin/src/components/template/Footer.jsx
@@ -0,0 +1,8 @@
+import './Footer'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span>Desenvolvido com <i className="fa fa-heart text-danger"></i> por 
+        <strong> Rafael Teixeira</strong></span>
+    </footer>
